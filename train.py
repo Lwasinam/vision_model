65a5667f2ec46f26fe47c6d8b7fb8e6fa49d4a20
Lwasinam <lwasinamdilli@gmail.com>
2024-02-08 15:35:17 +0100

    chnage

--- train.py
+++ train.py
@@ -230,7 +230,7 @@ def train_model(config):
     # Tensorboard
     writer = SummaryWriter(config['experiment_name'])
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-5, eps=1e-9)
     scheduler = StepLR(optimizer, step_size=1000, gamma=0.95) 
 
     # If the user specified a model to preload before training, load it
